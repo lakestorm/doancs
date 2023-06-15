d85366f6f7a6a32270816890ebb57a328c4467ac
lakestorm <baolathe0108@gmail.com>
2023-06-15 17:46:31 +0700

    dsds

--- js/hhhhh.js
+++ js/hhhhh.js
@@ -28,16 +28,33 @@ function signup (e) {
             alert("Sai email hoặc mật khẩu");
         }
     }
+    function checkemail(){
+        var regExp = /^[A-Za-z][\w$.]+@[\w]+\.\w+$/;
+        var email = document.getElementById("email").value;
+        if (regExp.test(email)) 
+            alert('Email hợp lệ!'); 
+          else 
+              alert('email không hợp lệ!');
+      }
+      function checkpassword(){
+        var regExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        var password = document.getElementById("password").value;
+        if (regExp.test(password)) 
+            alert('Mật khẩu hợp lệ'); 
+          else 
+              alert('Mật khẩu tối thiểu tám ký tự, ít nhất một chữ cái và một số');
+      }
 
-    function kiemtramatkhau(){  
-        var firstpassword=document.f1.password.value;  
-        var secondpassword=document.f1.password2.value;  
+      function matchpass(){  
           
+        var firstpassword = document.getElementById("password").value;
+        var secondpassword = document.getElementById("password2").value;
+        
         if(firstpassword==secondpassword){  
-        return true;  
+            alert("Nhập đúng")
         }  
         else{  
-        alert("password must be same!");  
+        alert("Không giống mật khẩu gốc");  
         return false;  
         }  
         }  
